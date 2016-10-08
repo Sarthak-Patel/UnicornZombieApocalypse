3c8f963e30cc7c57986742da1a50bead3064915c
Tyler Fameli <tylerfameli@gmail.com>
2016-10-08 13:00:31 -0400

    Menu screens fully completed/Death now completes to ending screen

--- Assets/GoogleVR/Legacy/Scripts/Internal/VRDevices/DestroyCollision.cs
+++ Assets/GoogleVR/Legacy/Scripts/Internal/VRDevices/DestroyCollision.cs
@@ -2,17 +2,19 @@
 using UnityEngine.UI;
 using System.Collections;
 
+
 public class DestroyCollision : MonoBehaviour {
     public Text countText;
+    public Text countText2;
     private int count;
     public AudioClip nay;
-
     void Start()
     {
         GetComponent<AudioSource>().playOnAwake = false;
         GetComponent<AudioSource>().clip = nay;
         count = 0;
         countText.text = "Count: " + count.ToString();
+        countText2.text = "Count: " + count.ToString();
     }
 	void OnCollisionEnter(Collision col)
     {
@@ -21,6 +23,7 @@ public class DestroyCollision : MonoBehaviour {
             Destroy(col.gameObject);
             count = count + 1;
             countText.text = "Count: " + count.ToString ();
+            countText2.text = "Count: " + count.ToString();
             GetComponent<AudioSource>().Play();
         }
     }
@@ -29,5 +32,6 @@ public class DestroyCollision : MonoBehaviour {
     {
         count = 0;
         countText.text = "Count: " + count.ToString();
+        countText2.text = "Count: " + count.ToString();
     }
 }

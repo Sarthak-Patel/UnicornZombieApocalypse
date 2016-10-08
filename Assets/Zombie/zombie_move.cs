7fa5d37d6457c5a8c3cce3cae6a083a8bbae119a
Tyler Fameli <tylerfameli@gmail.com>
2016-10-08 07:04:32 -0400

    Added score/collision

--- Assets/Zombie/zombie_move.cs
+++ Assets/Zombie/zombie_move.cs
@@ -6,11 +6,12 @@ public class zombie_move : MonoBehaviour {
 	public Transform target;
 	public float speed;
 
+
 	// Use this for initialization
 	void Start () {
 		const int HEIGHT = 3;
-		int x = Random.Range(200,400);
-		int z = Random.Range(200,400);
+		int x = Random.Range(300,500);
+		int z = Random.Range(300,500);
 
 		transform.position = new Vector3(x,HEIGHT,z);
 	}

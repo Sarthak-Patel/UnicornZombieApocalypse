b7b2c5e373f64ad525fa4ffc568df7385b4dd5a7
Tyler Fameli <tylerfameli@gmail.com>
2016-10-08 12:01:33 -0400

    Fixed Button Issues

---
+++ Assets/MainScene/StartGame.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class StartGame : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text StartTheGame; // add an image as child to your button object and set its image type to Filled. Assign it to this field in inspector.
+    public bool isEntered = false;
+    RectTransform rt;
+    Button _button;
+    float timeElapsed;
+    Image cursor;
+    // Use this for initialization
+    void Awake()
+    {
+        _button = GetComponent<Button>();
+        rt = GetComponent<RectTransform>();
+
+    }
+
+    float GazeActivationTime = 3;
+
+    void Update()
+    {
+        if (isEntered)
+        {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed >= GazeActivationTime)
+            {
+                timeElapsed = 0;
+                _button.onClick.Invoke();
+                isEntered = false;
+            }
+        }
+        else
+        {
+            timeElapsed = 0;
+        }
+    }
+
+    #region IPointerEnterHandler implementation
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isEntered = true;
+    }
+
+    #endregion
+
+    #region IPointerExitHandler implementation
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isEntered = false;
+    }
+}
+    #endregion

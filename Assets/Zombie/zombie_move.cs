fb54ed697ce15342effd55c839268ed220c01b43
Benjamin Carroll <b3ncarroll@gmail.com>
2016-10-08 07:19:26 -0400

    movement for a zombie

--- Assets/Zombie/zombie_move.cs
+++ Assets/Zombie/zombie_move.cs
@@ -5,18 +5,28 @@ public class zombie_move : MonoBehaviour {
 	
 	public Transform target;
 	public float speed;
+	public Camera m_Camera;
+
 
 	// Use this for initialization
 	void Start () {
-		const int HEIGHT = 3;
-		int x = Random.Range(200,400);
-		int z = Random.Range(200,400);
+		// spawn randomly
+		const int HEIGHT = 2;
+		int x = Random.Range(300,500);
+		int z = Random.Range(300,500);
 
 		transform.position = new Vector3(x,HEIGHT,z);
+
+
 	}
 
-	void Update() {
+	void Update() {		
+		// move towards player
 		float step = speed * Time.deltaTime;
 		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+		// face player
+		transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,m_Camera.transform.rotation * Vector3.up);
+
 	}
 }

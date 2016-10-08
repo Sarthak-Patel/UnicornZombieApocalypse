9e3294123d7e1c7a22df1f0af0b4b7408d272a26
Tyler Fameli <tylerfameli@gmail.com>
2016-10-08 06:07:42 -0400

    DESTRUCTO ZOMBO

---
+++ Assets/GoogleVR/Legacy/Scripts/Internal/VRDevices/DestroyCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCollision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Zombie")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}

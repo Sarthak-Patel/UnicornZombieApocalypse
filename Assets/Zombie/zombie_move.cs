acfb205d7481b8da91dcf65275d366a385654039
Benjamin Carroll <b3ncarroll@gmail.com>
2016-10-08 07:30:30 -0400

    fixing conflicts

--- Assets/Zombie/zombie_move.cs
+++ Assets/Zombie/zombie_move.cs
@@ -11,12 +11,10 @@ public class zombie_move : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
+
 		// spawn randomly
 		const int HEIGHT = 2;
-=======
 		const int HEIGHT = 3;
->>>>>>> 7fa5d37d6457c5a8c3cce3cae6a083a8bbae119a
 		int x = Random.Range(300,500);
 		int z = Random.Range(300,500);
 

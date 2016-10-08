3d36866050dbf532ea0de44d68aba0da5a194203
Benjamin Carroll <b3ncarroll@gmail.com>
2016-10-08 12:18:25 -0400

    began reset

--- Assets/scripts/ResetListener.cs
+++ Assets/scripts/ResetListener.cs
@@ -2,11 +2,15 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class ResetListener : MonoBehaviour {
+public class ResetListener : MagnetSensor {
 
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	void OnCardboardTrigger(){
+		SceneManager.LoadScene("Scene_Name");
 	}
 	
 	// Update is called once per frame

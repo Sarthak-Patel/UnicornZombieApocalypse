b7322321d98a9546b817da7c21e7c28011ef897a
Benjamin Carroll <b3ncarroll@gmail.com>
2016-10-08 06:11:40 -0400

    added random spawn for 1 zombie

---
+++ Assets/Zombie/zombie_move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zombie_move : MonoBehaviour {
+	
+	public Transform target;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		const int HEIGHT = 3;
+		int x = Random.Range(200,400);
+		int z = Random.Range(200,400);
+
+		transform.position = new Vector3(x,HEIGHT,z);
+	}
+
+	void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+}
